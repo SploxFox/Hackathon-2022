13481c9e8c8f0c1f9da933d505f52bc9b035e965
Serena <sjtl2004@gmail.com>
2022-11-15 19:22:51 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -12,35 +12,93 @@
    Serial1.print().
 */
 #include <Arduino.h>
+#include <SPIMemory.h>
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include <stdio.h>
+#include <math.h>
 
-int RXLED = 17;  // The RX LED has a defined Arduino pin
-// Note: The TX LED was not so lucky, we'll need to use pre-defined
-// macros (TXLED1, TXLED0) to control that.
-// (We could use the same macros for the RX LED too -- RXLED1,
-//  and RXLED0.)
 
-void setup()
-{
-  pinMode(RXLED, OUTPUT);  // Set RX LED as an output
-  // TX LED is set as an output behind the scenes
+SPIFlash flash;
 
+struct FlightData {
+  double pressure;
+  double temperature;
+  double time;
+};
+
+FlightData data;
+uint32_t addr = 0;
+
+void setup() {
   Serial.begin(9600); //This pipes to the serial monitor
   Serial.println("Initialize Serial Monitor");
 
   Serial1.begin(9600); //This is the UART, pipes to sensors attached to board
   Serial1.println("Initialize Serial Hardware UART Pins");
+
+  flash.begin();
 }
 
-void loop()
-{
-  Serial.println("Hello world!");  // Print "Hello World" to the Serial Monitor
-  Serial1.println("Hello! Can anybody hear me?");  // Print "Hello!" over hardware UART
+// https://forum.arduino.cc/t/printing-a-double-variable/44327/7
+void printDouble( double val, byte precision){
+  // prints val with number of decimal places determine by precision
+  // precision is a number from 0 to 6 indicating the desired decimial places
+  // example: printDouble( 3.1415, 2); // prints 3.14 (two decimal places)
 
-  digitalWrite(RXLED, LOW);   // set the RX LED ON
-  TXLED0; //TX LED is not tied to a normally controlled pin so a macro is needed, turn LED OFF
-  delay(1000);              // wait for a second
-
-  digitalWrite(RXLED, HIGH);    // set the RX LED OFF
-  TXLED1; //TX LED macro to turn LED ON
-  delay(1000);              // wait for a second
+  Serial.print (int(val));  //prints the int part
+  if( precision > 0) {
+    Serial.print("."); // print the decimal point
+    unsigned long frac;
+    unsigned long mult = 1;
+    byte padding = precision -1;
+    while(precision--)
+       mult *=10;
+       
+    if(val >= 0)
+      frac = (val - int(val)) * mult;
+    else
+      frac = (int(val)- val ) * mult;
+    unsigned long frac1 = frac;
+    while( frac1 /= 10 )
+      padding--;
+    while(  padding--)
+      Serial.print("0");
+    Serial.print(frac,DEC) ;
+  }
 }
+
+void loop() {
+  /*while (flash.readByte(addr) != 0xFF) {
+    addr++;
+  }
+
+  Serial.println("Found open address %06x", addr);*/
+
+  if (!!Serial) {
+    // In flight
+
+    flash.writeByte(addr, 0);
+    flash.writeAnything(addr + 1, data);
+    addr += sizeof(FlightData) + 1;
+  } else {
+    // Plugged in
+
+    Serial.println("Time,Pressure,Temperature");
+    while (flash.readByte(addr) != 0x00) {
+      flash.readByte(addr);
+      flash.readAnything(addr + 1, data);
+      
+      Serial.println(data.time, data.pressure, data.temperature);
+      addr += sizeof(FlightData) + 1;
+    }
+
+    while (true) {
+
+    }
+  }
+}
+
+
+
+
